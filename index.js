290fc0c62666e0efa8e83fc9db5475fb88e57e1b
Hizkia Natanael <hizkianata1911gmail.com>
2024-10-26 00:08:23 +0700

    Menambahkan Switch Case untuk Perintah CLI

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const app = require("./app");
 
 const args = process.argv.slice(2);
-
 const command = args[0];
 
+// Switch case untuk menjalankan perintah sesuai dengan input
 switch (command) {
-    case "make-folder":
-        app.makeFolder()
-        break;
+  case "make-folder":
+    app.makeFolder(args[1]); // Nama folder bisa diambil dari args[1]
+    break;
 
-    default:
-        throw Error("Invalid command")
-        break;
+  case "make-file":
+    app.makeFile(args[1], args[2]); // Nama file dan konten bisa diambil dari args[1] dan args[2]
+    break;
+
+  case "ext-sorter":
+    app.extSorter();
+    break;
+
+  case "read-folder":
+    app.readFolder(args[1]); // Nama folder yang akan dibaca bisa diambil dari args[1]
+    break;
+
+  case "read-file":
+    app.readFile(args[1]); // Nama file yang akan dibaca diambil dari args[1]
+    break;
+
+  default:
+    throw Error("Invalid command");
 }
- 
-
